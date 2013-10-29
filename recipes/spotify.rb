88d6ced83e65ccd6dc83ea01c3569955cf935445
nat <nat.lownes@gmail.com>
2013-10-29 10:11:56 -0400

    spotify install for debian distros

---
+++ recipes/spotify.rb
@@ -0,0 +1,23 @@
+execute "apt-get-update" do
+  command "apt-get update --fix-missing"
+
+  action :nothing
+end
+
+if node.platform_family == 'debian'
+  file "/etc/apt/sources.list.d/spotify.list" do
+    content "deb http://repository.spotify.com stable non-free"
+
+    notifies :run, "execute[apt-get-update]", :immediately
+  end
+
+  execute "install-spotify-key" do
+    command "apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59"
+
+    notifies :run, "execute[apt-get-update]", :immediately
+  end
+
+  package 'spotify-client'
+
+end
+
