7756a4896fefd405067102e46ae6bd4e47808e28
nat <nat.lownes@gmail.com>
2013-10-29 18:35:22 -0400

    xscreensaver things

--- recipes/xscreensaver.rb
+++ recipes/xscreensaver.rb
@@ -2,8 +2,19 @@ extend Nat::UserHelpers
 username = user_name()
 home_dir = home_dir()
 
+directory "#{home_dir}/.config/xscreensaver" do
+  recursive true
+  owner username
+end
+
 template "#{home_dir}/.xscreensaver" do
   source "xscreensaver/config"
+
+  owner username
+end
+
+template "#{home_dir}/.config/xscreensaver/rc" do
+  source "xscreensaver/rc"
 
   owner username
 end
