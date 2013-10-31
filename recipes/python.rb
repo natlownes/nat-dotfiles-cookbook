adf33f10a74eadbd17cfb1c115eacc63c8a3a9ff
nat <nat.lownes@gmail.com>
2013-10-31 08:08:36 -0400

    python - install setuptools and pip

--- recipes/python.rb
+++ recipes/python.rb
@@ -1,6 +1,14 @@
 
+execute "install-setuptools" do
+  command "wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | python"
+end
+
+execute "install-pip" do
+  command "https://raw.github.com/pypa/pip/master/contrib/get-pip.py -O - | python"
+end
+
 execute "install-pyflakes-globally" do
   command "pip install --upgrade pyflakes"
-  only_if "type python > /dev/null"
+  only_if "type python > /dev/null && type pip > /dev/null"
 end
 
