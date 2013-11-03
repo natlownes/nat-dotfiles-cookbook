b672a1a849646b999329459610ce726cb00d1c04
nat <nat.lownes@gmail.com>
2013-11-03 11:37:30 -0500

    updated key_mount script, removed old

--- files/default/scripts/key_mount.bash
+++ files/default/scripts/key_mount.bash
@@ -4,14 +4,24 @@ action=$1
 user=`who -m | awk '{print $1;}'`
 userid=$(awk -F"[=(]" '{print $2,$4}' <(id "$user"))
 home_dir="/home/$user"
+mount_dir_root="$HOME/opt/local/mnt"
+images=('keys.tc' 'src-secure.tc')
+mount_points=('home')
+mount_dirs=("${images[@]}" "${mount_points[@]}")
+images_root="$HOME/opt/local/mnt/home"
 
 if [[ "mount" == "$action" ]]; then
-  mkdir -p $HOME/opt/local/mnt/home
-  mkdir -p $HOME/opt/local/mnt/keys
-  mkdir -p $HOME/opt/local/mnt/src
+  [[ ! -d $mount_dir_root ]] && mkdir -p $mount_dir_root
+  for dir_name in ${mount_dirs[@]}; do
+    mount_point="$mount_dir_root/$dir_name"
+    [[ ! -d $mount_point ]] && mkdir "$mount_point"
+  done
+  # mount physical
   sudo mount -o nosuid,uid="${userid% *}",gid="${userid#* }" /dev/disk/by-label/HOME $HOME/opt/local/mnt/home || exit 1
-  truecrypt -t --mount --fs-options=force,nosuid,uid="${userid% *}",gid="${userid#* }" $HOME/opt/local/mnt/home/keys.tc ~/opt/local/mnt/keys
-  truecrypt -t --mount --fs-options=force,nosuid,uid="${userid% *}",gid="${userid#* }" $HOME/opt/local/mnt/home/src-secure.tc ~/opt/local/mnt/src
+
+  for image_name in ${images[@]}; do
+    truecrypt -t --mount --fs-options=force,nosuid,uid="${userid% *}",gid="${userid#* }" "$images_root/$image_name" "$mount_dir_root/$image_name"
+  done
 fi
 
 if [[ "umount" == "$action" ]]; then
