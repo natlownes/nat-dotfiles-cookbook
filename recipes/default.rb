f2e00bae0659899a26a5ec25a4c9a14f9f37bc61
nat <nat.lownes@gmail.com>
2013-11-03 14:53:38 -0500

    ensure package indexes upto date

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,20 @@ extend Nat::UserHelpers
 username = user_name()
 user_dir = home_dir()
 
+package_manager_update_command = value_for_platform({
+  ['debian', 'ubuntu'] => {
+    'default' => 'apt-get update --fix-missing'
+  },
+  'mac_os_x' => {
+    'default' => 'port -d selfupdate'
+  }
+})
+
+execute "ready-package-manager" do
+  command package_manager_update_command
+  action :nothing
+end.run_action(:run)
+
 include_recipe 'nat::zsh'
 
 user username do
