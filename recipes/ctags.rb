f07923cfa9a28fb8a5d3081575f28cd13bb9c65a
nat <nat.lownes@gmail.com>
2013-11-03 15:31:43 -0500

    ctags x-platform package name

--- recipes/ctags.rb
+++ recipes/ctags.rb
@@ -2,7 +2,16 @@ extend Nat::UserHelpers
 username = user_name()
 user_dir = home_dir()
 
-package 'exuberant-ctags'
+package_name = value_for_platform({
+  ['debian', 'ubuntu'] => {
+    'default' => 'exuberant-ctags'
+  },
+  'mac_os_x' => {
+    'default' => 'ctags'
+  }
+})
+
+package package_name
 
 template "#{user_dir}/.ctags" do
   source "ctags/ctags"
