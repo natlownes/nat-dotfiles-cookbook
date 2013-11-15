adab4807d0f22f9a96b25a01be31ed65432b97a5
nat <nat.lownes@gmail.com>
2013-11-15 16:58:52 -0500

    adding name/email to vistar clone script

--- files/default/scripts/vistar_clone_project.bash
+++ files/default/scripts/vistar_clone_project.bash
@@ -4,6 +4,9 @@ src_dir="$HOME/src/vistar"
 url_base="ssh://gerrit.vistarmedia.com:29418/"
 project_name=$1
 
+user_real_name="nat"
+user_vistar_email="nat@vistarmedia.com"
+
 if [ -z "$project_name" ]; then
   echo "requires a project name"
   exit 1
@@ -13,4 +16,5 @@ project_url="${url_base}${project_name}.git"
 
 mkdir -p $src_dir
 
-pushd $src_dir && git clone $project_url && popd
+pushd $src_dir && git clone $project_url && git config user.name $user_real_name
+&& git config user.email $user_vistar_email && popd
