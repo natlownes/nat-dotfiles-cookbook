3d09b2796facd7bbecc0f3ce4af1f64ff6de0c47
nat <nat.lownes@gmail.com>
2013-11-18 15:43:37 -0500

    update vistar clone project script

--- files/default/scripts/vistar_clone_project.bash
+++ files/default/scripts/vistar_clone_project.bash
@@ -3,6 +3,7 @@
 src_dir="$HOME/src/vistar"
 url_base="ssh://gerrit.vistarmedia.com:29418/"
 project_name=$1
+destination_dir=$src_dir/$project_name
 
 user_real_name="nat"
 user_vistar_email="nat@vistarmedia.com"
@@ -14,6 +15,15 @@ fi
 
 project_url="${url_base}${project_name}.git"
 
+function git_clone () {
+  git clone $project_url $destination_dir
+}
+
+
+function git_update_user_info () {
+cd $destination_dir && git config user.name $user_real_name && git config user.email $user_vistar_email
+}
+
 mkdir -p $src_dir
 
-pushd $src_dir && git clone $project_url && git config user.name $user_real_name && git config user.email $user_vistar_email && popd
+pushd $src_dir && git_clone && git_update_user_info && popd
