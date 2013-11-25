504b1225f6c8774af928910288e8fee210bc1216
nat <nat.lownes@gmail.com>
2013-11-24 22:36:52 -0500

    pgp

---
+++ recipes/pgp.rb
@@ -0,0 +1,19 @@
+extend Nat::UserHelpers
+username = user_name()
+home_dir = home_dir()
+
+package 'gnupg2'
+
+directory "#{home_dir}/.gnupg" do
+  owner username
+  recursive true
+  action :create
+end
+
+template "#{home_dir}/.gnupg/gpg.config" do
+  source "pgp/gpg.conf.erb"
+  owner username
+end
+
+
+
