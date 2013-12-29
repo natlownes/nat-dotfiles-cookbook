a8db2d429261a01ba2ee96c9c6f6f29aa8425d37
nat <nat.lownes@gmail.com>
2013-12-28 20:58:02 -0500

    python recipe fix found on 13.04

--- recipes/python.rb
+++ recipes/python.rb
@@ -7,31 +7,33 @@ package_name = value_for_platform({
   }
 })
 
-package package_name
+package package_name do
+  action :install
+end
 
 execute "install-setuptools" do
-  command "wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | python"
+  command "wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | #{package_name}"
   not_if "type easy_install > /dev/null"
 end
 
 execute "install-pip" do
-  command "wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py -O - | python"
+  command "wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py -O - | #{package_name}"
   not_if "type pip > /dev/null"
 end
 
 execute "install-virtualenv" do
   command "pip install --upgrade virtualenv"
-  only_if "type python > /dev/null && type pip > /dev/null"
+  only_if "type #{package_name} > /dev/null && type pip > /dev/null"
 end
 
 execute "install-pyflakes-globally" do
   command "pip install --upgrade pyflakes"
-  only_if "type python > /dev/null && type pip > /dev/null"
+  only_if "type #{package_name} > /dev/null && type pip > /dev/null"
 end
 
 execute "install-flake8-globally" do
   command "pip install --upgrade flake8"
-  only_if "type python > /dev/null && type pip > /dev/null"
+  only_if "type #{package_name} > /dev/null && type pip > /dev/null"
 end
 
 
