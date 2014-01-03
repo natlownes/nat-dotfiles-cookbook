3dee795ab5747dc1b254f15a840569d1b481be1e
nat <nat.lownes@gmail.com>
2014-01-02 20:45:51 -0500

    python

--- recipes/python.rb
+++ recipes/python.rb
@@ -7,17 +7,26 @@ package_name = value_for_platform({
   }
 })
 
+exec_name = value_for_platform({
+  ['debian', 'ubuntu'] => {
+    'default' => 'python2.7'
+  },
+  'mac_os_x' => {
+    'default' => 'python2.7'
+  }
+})
+
 package package_name do
   action :install
 end
 
 execute "install-setuptools" do
-  command "wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | #{package_name}"
+  command "wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | #{exec_name}"
   not_if "type easy_install > /dev/null"
 end
 
 execute "install-pip" do
-  command "wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py -O - | #{package_name}"
+  command "wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py -O - | #{exec_name}"
   not_if "type pip > /dev/null"
 end
 
