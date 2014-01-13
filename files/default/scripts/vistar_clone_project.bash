16ad250c2506395f08983e8592e673cd6a4e991f
nat <nat.lownes@gmail.com>
2014-01-13 17:41:06 -0500

    work clone script to add metal as remote

--- files/default/scripts/vistar_clone_project.bash
+++ files/default/scripts/vistar_clone_project.bash
@@ -4,6 +4,7 @@ src_dir="$HOME/src/vistar"
 url_base="ssh://gerrit.vistarmedia.com:29418/"
 project_name=$1
 destination_dir=$src_dir/$project_name
+metal_machine="192.168.11.13"
 
 user_real_name="nat"
 user_vistar_email="nat@vistarmedia.com"
@@ -24,6 +25,10 @@ function git_update_user_info () {
 cd $destination_dir && git config user.name $user_real_name && git config user.email $user_vistar_email
 }
 
+function git_update_home_remote () {
+cd $destination_dir && git remote add doublev ssh://$metal_machine/home/nat/src/vistar/$project_name
+}
+
 mkdir -p $src_dir
 
-pushd $src_dir && git_clone && git_update_user_info && popd
+pushd $src_dir && git_clone && git_update_user_info && git_update_home_remote && popd
