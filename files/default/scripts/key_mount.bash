e365b14812984a822054697856160443e510bceb
nat <nat.lownes@gmail.com>
2014-03-19 17:25:11 -0400

    mount script

--- files/default/scripts/key_mount.bash
+++ files/default/scripts/key_mount.bash
@@ -4,28 +4,30 @@ action=$1
 user=`whoami`
 userid=$(awk -F"[=(]" '{print $2,$4}' <(id "$user"))
 home_dir="/home/$user"
-mount_dir_root="$HOME/opt/local/mnt"
-images=('keys.tc' 'src-secure.tc')
+mount_dir_root="$HOME/opt/local/mnt/home"
 mount_points=('home')
-mount_dirs=("${images[@]}" "${mount_points[@]}")
-images_root="$HOME/opt/local/mnt/home"
+mount_dirs=("${mount_points[@]}")
 
 if [[ "mount" == "$action" ]]; then
   [[ ! -d $mount_dir_root ]] && mkdir -p $mount_dir_root
   for dir_name in ${mount_dirs[@]}; do
     mount_point="$mount_dir_root/$dir_name"
+    encmount_point="$mount_dir_root/.$dir_name"
     [[ ! -d $mount_point ]] && mkdir "$mount_point"
+    [[ ! -d $encmount_point ]] && mkdir "$encmount_point"
   done
   # mount physical
-  sudo mount -o nosuid,uid="${userid% *}",gid="${userid#* }" /dev/disk/by-label/HOME $HOME/opt/local/mnt/home || exit 1
+  sudo mount -o nosuid,uid="${userid% *}",gid="${userid#* }" /dev/disk/by-partlabel/home $HOME/opt/local/mnt/home || exit 1
 
-  for image_name in ${images[@]}; do
-    truecrypt -t --mount --fs-options=force,nosuid,uid="${userid% *}",gid="${userid#* }" "$images_root/$image_name" "$mount_dir_root/$image_name"
+  for dir_name in ${mount_dirs[@]}; do
+    mount_point="$mount_dir_root/$dir_name"
+    encmount_point="$mount_dir_root/.$dir_name"
+    encfs $encmount_point $mount_point
   done
 fi
 
 if [[ "umount" == "$action" ]]; then
-  truecrypt -t -d
-  sudo umount $HOME/opt/local/mnt/home
+  sudo umount -fl $HOME/opt/local/mnt/home/home
+  sudo umount -fl $HOME/opt/local/mnt/home
 fi
 
