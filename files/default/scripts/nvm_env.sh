fd56e45ab285129961e5070f150264edc2453d01
nat <nat.lownes@gmail.com>
2014-08-31 13:10:25 -0400

    nvm script for darwin and linux

--- files/default/scripts/nvm_env.sh
+++ files/default/scripts/nvm_env.sh
@@ -1,7 +1,15 @@
 source ~/.bin/source_bash.zsh
 
+os=$(uname)
 export NVM_DIR=$HOME/.nvm
 
 if [ -r $NVM_DIR ]; then
+  # TODO:  figure out wtf
+  if [ $os = 'Linux' ]; then
     source_bash $NVM_DIR/nvm.sh
+  fi
+
+  if [ $os = 'Darwin' ]; then
+    source $NVM_DIR/nvm.sh
+  fi
 fi
