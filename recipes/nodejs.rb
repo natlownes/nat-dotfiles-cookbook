07ccb9bd7480a58efc9c3618448385fe7e5308e0
nat <nat.lownes@gmail.com>
2014-10-25 11:28:57 -0400

    nvm install update

--- recipes/nodejs.rb
+++ recipes/nodejs.rb
@@ -8,6 +8,6 @@ execute 'install-nvm' do
   environment({
     'NVM_DIR' => "#{home_dir}/.nvm"
   })
-  command 'curl https://raw.github.com/creationix/nvm/master/install.sh | sh'
+  command 'curl https://raw.githubusercontent.com/creationix/nvm/v0.17.3/install.sh | bash'
   user    username
 end
