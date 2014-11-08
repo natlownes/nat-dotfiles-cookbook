83dda232bba4ea07b884e24b4971cfae6b019967
nat <nat.lownes@gmail.com>
2014-11-08 09:36:00 -0500

    add chromebook keybindings

--- recipes/chromebook.rb
+++ recipes/chromebook.rb
@@ -3,6 +3,8 @@ extend Nat::UserHelpers
 username = user_name()
 home_dir = home_dir()
 
+tmpdir = ::Dir.mktmpdir
+
 # kernel install, for reference
 # sudo wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.17-rc1-utopic/linux-headers-3.17.0-031700rc1-generic_3.17.0-031700rc1.201408161335_amd64.deb
 # sudo wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.17-rc1-utopic/linux-headers-3.17.0-031700rc1_3.17.0-031700rc1.201408161335_all.deb
@@ -16,4 +18,15 @@ end
 
 template '/etc/modprobe.d/blacklist-chromeos-laptop.conf' do
   source 'chromebook/blacklist-chromeos-laptop.conf.erb'
+end
+
+xkbscript_path = ::File.join(tmpdir, 'xkb_bindings.bash')
+
+template xkbscript_path do
+  source 'chromebook/xkb_bindings.bash'
+  mode   '700'
+end
+
+bash 'apply-chromebook-keybindings' do
+  code "#{xkbscript_path}"
 end
