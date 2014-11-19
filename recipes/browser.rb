bb265e98d1cc6a3eec6f4ab0a4913e280f8b52b9
nat <nat.lownes@gmail.com>
2014-11-19 10:44:48 -0500

    firefox

--- recipes/browser.rb
+++ recipes/browser.rb
@@ -15,12 +15,37 @@ end
 remote_file "#{browser_package_path}/google-chrome-stable_current_amd64.deb" do
   source "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
   use_etag true
+  owner username
 
   notifies :run, "execute[install-googs-chrome]", :immediately
 end
 
 execute "install-googs-chrome" do
   command "dpkg -i  #{browser_package_path}/google-chrome-stable_current_amd64.deb"
+
+  action :nothing
+end
+
+remote_file "#{browser_package_path}/firefox.tar.bz2" do
+  source "https://download.mozilla.org/?product=firefox-33.1.1-SSL&os=linux64&lang=en-US"
+  use_etag true
+  owner username
+
+  notifies :run, 'execute[install-firefox]', :immediately
+end
+
+execute 'install-firefox' do
+  command "tar -C #{user_dir} -xjvf #{browser_package_path}/firefox.tar.bz2"
+  user username
+
+  action :nothing
+  notifies :create, 'link[firefox]', :immediately
+end
+
+link 'firefox' do
+  owner username
+  target_file "#{user_dir}/.bin/firefox"
+  to          "#{user_dir}/firefox/firefox"
 
   action :nothing
 end
