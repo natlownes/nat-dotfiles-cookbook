f491cebee4f9ee5814b5435b5690cec3fbb84714
nat <nat.lownes@gmail.com>
2014-12-07 17:28:18 -0500

    typo

--- templates/default/i3/i3status.bash
+++ templates/default/i3/i3status.bash
@@ -4,7 +4,7 @@ lightblue='\033[1;34m'
 red='\033[0;31m'
 nocolor='\033[0m'
 
-i3status | while read -e line :
+i3status | while read -e line
 do
   current_weather="$(bash <%= @home_dir %>/.weather/current.bash)"
   echo "$current_weather | $line" || exit 1
