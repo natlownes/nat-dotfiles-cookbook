9f7f77b5f714e2bc4dc7c714c97d00d03c3a9659
nat <nat.lownes@gmail.com>
2014-12-07 16:09:17 -0500

    i3bar

--- templates/default/i3/i3status.bash
+++ templates/default/i3/i3status.bash
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-i3status | while :
+lightblue='\033[1;34m'
+red='\033[0;31m'
+nocolor='\033[0m'
+
+i3status | while read -e line :
 do
   current_weather="$(bash <%= @home_dir %>/.weather/current.bash)"
-  read line
   echo "$current_weather | $line" || exit 1
 done
