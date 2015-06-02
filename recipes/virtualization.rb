b6cd824251d33a639dfbe736d1deb4dbcc30b803
nat <nat.lownes@gmail.com>
2015-06-02 10:53:48 -0400

    install vagrant for reals

--- recipes/virtualization.rb
+++ recipes/virtualization.rb
@@ -5,21 +5,22 @@ home_dir = home_dir()
 include_recipe "nat::docker"
 
 
+package 'wget'
 package 'virtualbox'
 package 'redir'
 package 'cgroup-lite'
 
+
 remote_file "#{Chef::Config[:file_cache_path]}/vagrant.deb" do
   source "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.4.3_x86_64.deb"
   use_etag true
-
-  notifies :run, "execute[install-vagrant]", :immediately
 end
 
-execute "install-vagrant" do
-  command "dpkg -i  #{Chef::Config[:file_cache_path]}/vagrant.deb"
-
-  action :nothing
+bash 'install-vagrant' do
+  code %{
+    dpkg -i  #{Chef::Config[:file_cache_path]}/vagrant.deb
+  }
+  not_if 'which vagrant'
 end
 
 script "vagrant-install-lxc-provider" do
