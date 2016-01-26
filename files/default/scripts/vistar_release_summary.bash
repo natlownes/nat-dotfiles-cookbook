0d6614fec579a8bc94168c61b88b52c5dfd1f271
nat <nat.lownes@gmail.com>
2016-01-26 14:37:04 -0500

    note

--- files/default/scripts/vistar_release_summary.bash
+++ files/default/scripts/vistar_release_summary.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# git log --before=`date -dlast-monday +%Y-%m-%d` --oneline | head -1
+
 src_root="$HOME/src/vistar"
 
 projects="pub-ad-server platform geo-ui geo-rest vistar vistar-ui locus trafficking"
